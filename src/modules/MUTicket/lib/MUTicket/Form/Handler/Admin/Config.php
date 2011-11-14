d4fbc787f4a2afbaef9141d6ff006a6d5cf481b9
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2011-11-14 18:24:01 +0100

    overwriting handleCommand()

--- src/modules/MUTicket/lib/MUTicket/Form/Handler/Admin/Config.php
+++ src/modules/MUTicket/lib/MUTicket/Form/Handler/Admin/Config.php
@@ -36,11 +36,70 @@ class MUTicket_Form_Handler_Admin_Config extends MUTicket_Form_Handler_Base_Admi
     	
     	foreach ($groups as $group)  {
     	$supportgroups[] = array('value' => $group['name'], 'text' => $group['name']);
-    	}
+    	}    	
 
     	$config = $this->view->get_template_vars('config');
      	$config['supportergroupItems'] = $supportgroups;
      	$this->view->assign('config', $config);
 
     }
+    
+    /**
+     * Command event handler.
+     *
+     * This event handler is called when a command is issued by the user. Commands are typically something
+     * that originates from a {@link Zikula_Form_Plugin_Button} plugin. The passed args contains different properties
+     * depending on the command source, but you should at least find a <var>$args['commandName']</var>
+     * value indicating the name of the command. The command name is normally specified by the plugin
+     * that initiated the command.
+     * @see Zikula_Form_Plugin_Button
+     * @see Zikula_Form_Plugin_ImageButton
+     */
+    public function handleCommand(Zikula_Form_View $view, &$args)
+    {
+        if ($args['commandName'] == 'save') {
+            // check if all fields are valid
+            if (!$this->view->isValid()) {
+                return false;
+            }
+            
+            $supportergroup = ModUtil::getVar('MUTicket', 'supportergroup');
+            
+            // retrieve form data
+            $data = $this->view->getValues();
+            
+            // entered supportergroup, is it another?
+            $newsupportergroup = $data['config']['supportergroup'];
+            
+            // check if existing supporters deleting
+            $delete_supporter = $data['supporter']['delete_supporter'];
+
+            // update all module vars
+            if (!ModUtil::setVars('MUTicket', $data['config'])) {
+                return $this->view->setErrorMsg($this->__('Error! Failed to set configuration variables.'));
+            }
+
+            LogUtil::registerStatus($this->__('Done! Module configuration updated.'));
+                       
+            if($delete_supporter == 1 && $supportergroup != $newsupportergroup) {
+            	
+            	$repository = $this->entityManager->getRepository('MUTicket_Entity_Supporter');
+            	$objects = $repository->selectWhere();
+            	foreach($objects as $object) { 
+            	$this->entityManager->remove($object);
+            	$this->entityManager->flush();            	
+            	} 
+
+            	LogUtil::registerStatus($this->__('All supporters deleted. You have to save one!'));
+            }
+            
+        } else if ($args['commandName'] == 'cancel') {
+            // nothing to do there
+            }
+
+        // redirect back to the config page
+        $url = ModUtil::url('MUTicket', 'admin', 'config');
+        return $this->view->redirect($url);
+    }
+    
 }

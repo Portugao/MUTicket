2d0fb024815202483480aebbc2580db6531b0c23
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2012-06-05 16:19:01 +0200

    minor typo

--- src/modules/MUTicket/templates/plugins/block.muticketform.php
+++ src/modules/MUTicket/templates/plugins/block.muticketform.php
@@ -33,12 +33,8 @@ function smarty_block_muticketform($params, $content, $view)
             $classString = "class=\"$params[cssClass]\" ";
         }
         
-        $request = new Zikula_Request_Http();
-        
-        $parentid = $request->getGet()->filter('id', NULL, FILTER_SANITIZE_NUMBER_INT);
-
         if(strpos($action,"func=display")!==false) {
-        	$action = 'index.php?module=muticket&amp;type=user&amp;func=edit&amp;ot=ticket&amp;parent=' . $parentid;
+        	$action = 'index.php?module=muticket&amp;type=user&amp;func=edit&amp;ot=ticket';
         }
 
         $view->postRender();

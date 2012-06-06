da2471481c1aa951c359e57308a7ef4dc3ed6acb
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2012-06-06 15:51:31 +0200

    parent ticket in link

--- src/modules/MUTicket/lib/MUTicket/Controller/Ajax.php
+++ src/modules/MUTicket/lib/MUTicket/Controller/Ajax.php
@@ -21,7 +21,8 @@ class MUTicket_Controller_Ajax extends MUTicket_Controller_Base_Ajax
     	
     	$request = new Zikula_Request_Http();
     	$ticket = $request->getGet()->filter('ticket', 0, FILTER_SANITIZE_NUMBER_INT);
-    	$result = ModUtil::func($this->name, 'user', 'edit', array('ot' => 'rating', 'ticket' => $ticket));	
+    	$parent = $request->getGet()->filter('parent', NULL, FILTER_SANITIZE_NUMBER_INT);
+    	$result = ModUtil::func($this->name, 'user', 'edit', array('ot' => 'rating', 'ticket' => $ticket, 'parent' => $parent));	
     	
     	return $result;
     } 

aba59f6dd72606252b294ad7742631940bbcf28a
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2012-07-15 21:12:56 +0200

    try to fix callback issue; not working

--- src/modules/MUTicket/lib/MUTicket/Entity/Supporter.php
+++ src/modules/MUTicket/lib/MUTicket/Entity/Supporter.php
@@ -26,264 +26,270 @@ use DoctrineExtensions\StandardFields\Mapping\Annotation as ZK;
  */
 class MUTicket_Entity_Supporter extends MUTicket_Entity_Base_Supporter
 {
-    /**
-     * Collect available actions for this entity.
-     * TODO links don't work correctly
-     */
-    protected function prepareItemActions()
-    {
-        if (!empty($this->_actions)) {
-            return;
-        }
+	/**
+	 * Collect available actions for this entity.
+	 * TODO links don't work correctly
+	 */
+	protected function prepareItemActions()
+	{
+		if (!empty($this->_actions)) {
+			return;
+		}
 
-        $currentType = FormUtil::getPassedValue('type', 'user', 'GETPOST', FILTER_SANITIZE_STRING);
-        $currentFunc = FormUtil::getPassedValue('func', 'main', 'GETPOST', FILTER_SANITIZE_STRING);
-        $dom = ZLanguage::getModuleDomain('MUTicket');
-        if ($currentType == 'admin') {
-            if (in_array($currentFunc, array('main', 'view'))) {
-                    $this->_actions[] = array(
+		$currentType = FormUtil::getPassedValue('type', 'user', 'GETPOST', FILTER_SANITIZE_STRING);
+		$currentFunc = FormUtil::getPassedValue('func', 'main', 'GETPOST', FILTER_SANITIZE_STRING);
+		$dom = ZLanguage::getModuleDomain('MUTicket');
+		if ($currentType == 'admin') {
+			if (in_array($currentFunc, array('main', 'view'))) {
+				$this->_actions[] = array(
                         'url' => array('type' => 'admin', 'func' => 'rating', 'arguments' => array('ot' => 'ticket', 'rated' => 1, 'supporter' => $this['id'])),
                         'icon' => 'display',
                         'linkTitle' => __('Call statistic and rated tickets of this supporter', $dom),
                         'linkText' => __('Rating statictics', $dom)
-                    );
-                   /* $this->_actions[] = array(
-                        'url' => array('type' => 'admin', 'func' => 'display', 'arguments' => array('ot' => 'supporter', 'id' => $this['id'])),
-                        'icon' => 'display',
-                        'linkTitle' => str_replace('"', '', $this['username']),
-                        'linkText' => __('Details', $dom)
-                    );*/
-            }
+				);
+				/* $this->_actions[] = array(
+				 'url' => array('type' => 'admin', 'func' => 'display', 'arguments' => array('ot' => 'supporter', 'id' => $this['id'])),
+				 'icon' => 'display',
+				 'linkTitle' => str_replace('"', '', $this['username']),
+				 'linkText' => __('Details', $dom)
+				 );*/
+			}
 
-            if (in_array($currentFunc, array('main', 'view', 'display'))) {
-                if (SecurityUtil::checkPermission('MUTicket::', '.*', ACCESS_EDIT)) {
+			if (in_array($currentFunc, array('main', 'view', 'display'))) {
+				if (SecurityUtil::checkPermission('MUTicket::', '.*', ACCESS_EDIT)) {
 
-                    $this->_actions[] = array(
+					$this->_actions[] = array(
                         'url' => array('type' => 'admin', 'func' => 'edit', 'arguments' => array('ot' => 'supporter', 'id' => $this['id'])),
                         'icon' => 'edit',
                         'linkTitle' => __('Edit', $dom),
                         'linkText' => __('Edit', $dom)
-                    );
-                   /* $this->_actions[] = array(
-                        'url' => array('type' => 'admin', 'func' => 'edit', 'arguments' => array('ot' => 'supporter', 'astemplate' => $this['id'])),
-                        'icon' => 'saveas',
-                        'linkTitle' => __('Reuse for new item', $dom),
-                        'linkText' => __('Reuse', $dom)
-                    );*/
-                }
-                if (SecurityUtil::checkPermission('MUTicket::', '.*', ACCESS_DELETE)) {
-                    $this->_actions[] = array(
+					);
+					/* $this->_actions[] = array(
+					 'url' => array('type' => 'admin', 'func' => 'edit', 'arguments' => array('ot' => 'supporter', 'astemplate' => $this['id'])),
+					 'icon' => 'saveas',
+					 'linkTitle' => __('Reuse for new item', $dom),
+					 'linkText' => __('Reuse', $dom)
+					 );*/
+				}
+				if (SecurityUtil::checkPermission('MUTicket::', '.*', ACCESS_DELETE)) {
+					$this->_actions[] = array(
                         'url' => array('type' => 'admin', 'func' => 'delete', 'arguments' => array('ot' => 'supporter', 'id' => $this['id'])),
                         'icon' => 'delete',
                         'linkTitle' => __('Delete', $dom),
                         'linkText' => __('Delete', $dom)
-                    );
-                }
-            }
-            if ($currentFunc == 'display') {
-                   /* $this->_actions[] = array(
-                        'url' => array('type' => 'admin', 'func' => 'view', 'arguments' => array('ot' => 'supporter')),
-                        'icon' => 'back',
-                        'linkTitle' => __('Back to overview', $dom),
-                        'linkText' => __('Back to overview', $dom)
-                    );*/
-            }
-        }
-        if ($currentType == 'user') {
-            if (in_array($currentFunc, array('main', 'view'))) {
-                   /* $this->_actions[] = array(
-                        'url' => array('type' => 'user', 'func' => 'display', 'arguments' => array('ot' => 'supporter', 'id' => $this['id'])),
-                        'icon' => 'display',
-                        'linkTitle' => str_replace('"', '', $this['username']),
-                        'linkText' => __('Details', $dom)
-                    );*/
-            }
+					);
+				}
+			}
+			if ($currentFunc == 'display') {
+				/* $this->_actions[] = array(
+				 'url' => array('type' => 'admin', 'func' => 'view', 'arguments' => array('ot' => 'supporter')),
+				 'icon' => 'back',
+				 'linkTitle' => __('Back to overview', $dom),
+				 'linkText' => __('Back to overview', $dom)
+				 );*/
+			}
+		}
+		if ($currentType == 'user') {
+			if (in_array($currentFunc, array('main', 'view'))) {
+				/* $this->_actions[] = array(
+				 'url' => array('type' => 'user', 'func' => 'display', 'arguments' => array('ot' => 'supporter', 'id' => $this['id'])),
+				 'icon' => 'display',
+				 'linkTitle' => str_replace('"', '', $this['username']),
+				 'linkText' => __('Details', $dom)
+				 );*/
+			}
 
-            if (in_array($currentFunc, array('main', 'view', 'display'))) {
-                if (SecurityUtil::checkPermission('MUTicket::', '.*', ACCESS_EDIT)) {
+			if (in_array($currentFunc, array('main', 'view', 'display'))) {
+				if (SecurityUtil::checkPermission('MUTicket::', '.*', ACCESS_EDIT)) {
 
-                    $this->_actions[] = array(
+					$this->_actions[] = array(
                         'url' => array('type' => 'user', 'func' => 'edit', 'arguments' => array('ot' => 'supporter', 'id' => $this['id'])),
                         'icon' => 'edit',
                         'linkTitle' => __('Edit', $dom),
                         'linkText' => __('Edit', $dom)
-                    );
-                   /* $this->_actions[] = array(
-                        'url' => array('type' => 'user', 'func' => 'edit', 'arguments' => array('ot' => 'supporter', 'astemplate' => $this['id'])),
-                        'icon' => 'saveas',
-                        'linkTitle' => __('Reuse for new item', $dom),
-                        'linkText' => __('Reuse', $dom)
-                    );*/
-                }
-                if (SecurityUtil::checkPermission('MUTicket::', '.*', ACCESS_DELETE)) {
-                    $this->_actions[] = array(
+					);
+					/* $this->_actions[] = array(
+					 'url' => array('type' => 'user', 'func' => 'edit', 'arguments' => array('ot' => 'supporter', 'astemplate' => $this['id'])),
+					 'icon' => 'saveas',
+					 'linkTitle' => __('Reuse for new item', $dom),
+					 'linkText' => __('Reuse', $dom)
+					 );*/
+				}
+				if (SecurityUtil::checkPermission('MUTicket::', '.*', ACCESS_DELETE)) {
+					$this->_actions[] = array(
                         'url' => array('type' => 'user', 'func' => 'delete', 'arguments' => array('ot' => 'supporter', 'id' => $this['id'])),
                         'icon' => 'delete',
                         'linkTitle' => __('Delete', $dom),
                         'linkText' => __('Delete', $dom)
-                    );
-                }
-            }
-            if ($currentFunc == 'display') {
-                   /* $this->_actions[] = array(
-                        'url' => array('type' => 'user', 'func' => 'view', 'arguments' => array('ot' => 'supporter')),
-                        'icon' => 'back',
-                        'linkTitle' => __('Back to overview', $dom),
-                        'linkText' => __('Back to overview', $dom)
-                    );*/
-            }
-        }
-    }
+					);
+				}
+			}
+			if ($currentFunc == 'display') {
+				/* $this->_actions[] = array(
+				 'url' => array('type' => 'user', 'func' => 'view', 'arguments' => array('ot' => 'supporter')),
+				 'icon' => 'back',
+				 'linkTitle' => __('Back to overview', $dom),
+				 'linkText' => __('Back to overview', $dom)
+				 );*/
+			}
+		}
+	}
 
 
 
-    /**
-     * Post-Process the data after the entity has been constructed by the entity manager.
-     *
-     * @ORM\PostLoad
-     * @see MUTicket_Entity_Base_Supporter::performPostLoadCallback()
-     * @return void.
-     */
-    public function postLoadCallback()
-    {	
-    	$request = new Zikula_Request_Http();
-    	$type = $request->getGet()->filter('type', 'admin', FILTER_SANITIZE_STRING);
-    	$func = $request->getGet()->filter('func', 'view', FILTER_SANITIZE_STRING);
-    	$ot = $request->getGet()->filter('ot', 'ticket', FILTER_SANITIZE_STRING);
+	/**
+	 * Post-Process the data after the entity has been constructed by the entity manager.
+	 *
+	 * @ORM\PostLoad
+	 * @see MUTicket_Entity_Base_Supporter::performPostLoadCallback()
+	 * @return void.
+	 */
+	public function postLoadCallback()
+	{
+		$request = new Zikula_Request_Http();
+		$type = $request->getGet()->filter('type', 'admin', FILTER_SANITIZE_STRING);
+		$func = $request->getGet()->filter('func', 'view', FILTER_SANITIZE_STRING);
+		$ot = $request->getGet()->filter('ot', 'ticket', FILTER_SANITIZE_STRING);
 
-    	if ($type == 'admin') {
-    		if ($ot == 'supporter') {
-    			if ($func == 'view' || $func == 'edit') {
- 	
-    	             $this->getSupportcats();
-    	             $supportcats = $this->supportcats;
-    	             $supportcats = urldecode($supportcats);
-    	             $cats = unserialize($supportcats);
-    	             $this->setSupportcats($cats);
-    	        }
-    		}
-    	}	    
-        $this->performPostLoadCallback();
-    }
+		if ($type == 'admin') {
+			if ($ot == 'supporter') {
+				 
+				$this->getSupportcats();
+				$supportcats = $this->supportcats;
+				 
+				if ($func == 'view') {					 
+					$supportcats = urldecode($supportcats);
+					$cats = unserialize($supportcats);
+					$this->setSupportcats($cats);
+				}
+				if ($func == 'edit') {
+					$cats = unserialize($supportcats);
+					$supportcats = urldecode($supportcats);
+					$this->setSupportcats($cats);
+				}
+			}
+		}
+		$this->performPostLoadCallback();
+	}
 
-    /**
-     * Pre-Process the data prior to an insert operation.
-     *
-     * @ORM\PrePersist
-     * @see MUTicket_Entity_Base_Supporter::performPrePersistCallback()
-     * @return void.
-     */
-    public function prePersistCallback()
-    {
-    	$request = new Zikula_Request_Http();
-    	$type = $request->getGet()->filter('type', 'admin', FILTER_SANITIZE_STRING);
-    	$func = $request->getGet()->filter('func', 'view', FILTER_SANITIZE_STRING);
-    	$ot = $request->getGet()->filter('ot', 'ticket', FILTER_SANITIZE_STRING);
-    	
-    	if ($type == 'admin' && $func == 'edit' && $ot == 'supporter') {
-    	$this->getSupportcats();
-    	$supportercats = $this->supportcats;
-    	$cats = serialize($supportercats);
-    	$this->setSupportcats($cats);
-    	}
-        $this->performPrePersistCallback();
-    }
+	/**
+	 * Pre-Process the data prior to an insert operation.
+	 *
+	 * @ORM\PrePersist
+	 * @see MUTicket_Entity_Base_Supporter::performPrePersistCallback()
+	 * @return void.
+	 */
+	public function prePersistCallback()
+	{
+		$request = new Zikula_Request_Http();
+		$type = $request->getGet()->filter('type', 'admin', FILTER_SANITIZE_STRING);
+		$func = $request->getGet()->filter('func', 'view', FILTER_SANITIZE_STRING);
+		$ot = $request->getGet()->filter('ot', 'ticket', FILTER_SANITIZE_STRING);
+		 
+		if ($type == 'admin' && $func == 'edit' && $ot == 'supporter') {
+			$this->getSupportcats();
+			$supportercats = $this->supportcats;
+			$cats = serialize($supportercats);
+			$this->setSupportcats($cats);
+		}
+		$this->performPrePersistCallback();
+	}
 
-    /**
-     * Post-Process the data after an insert operation.
-     *
-     * @ORM\PostPersist
-     * @see MUTicket_Entity_Base_Supporter::performPostPersistCallback()
-     * @return void.
-     */
-    public function postPersistCallback()
-    {
-        $this->performPostPersistCallback();
-    }
+	/**
+	 * Post-Process the data after an insert operation.
+	 *
+	 * @ORM\PostPersist
+	 * @see MUTicket_Entity_Base_Supporter::performPostPersistCallback()
+	 * @return void.
+	 */
+	public function postPersistCallback()
+	{
+		$this->performPostPersistCallback();
+	}
 
-    /**
-     * Pre-Process the data prior a delete operation.
-     *
-     * @ORM\PreRemove
-     * @see MUTicket_Entity_Base_Supporter::performPreRemoveCallback()
-     * @return void.
-     */
-    public function preRemoveCallback()
-    {
-        $this->performPreRemoveCallback();
-    }
+	/**
+	 * Pre-Process the data prior a delete operation.
+	 *
+	 * @ORM\PreRemove
+	 * @see MUTicket_Entity_Base_Supporter::performPreRemoveCallback()
+	 * @return void.
+	 */
+	public function preRemoveCallback()
+	{
+		$this->performPreRemoveCallback();
+	}
 
-    /**
-     * Post-Process the data after a delete.
-     *
-     * @ORM\PostRemove
-     * @see MUTicket_Entity_Base_Supporter::performPostRemoveCallback()
-     * @return void
-     */
-    public function postRemoveCallback()
-    {
-        $this->performPostRemoveCallback();
-    }
+	/**
+	 * Post-Process the data after a delete.
+	 *
+	 * @ORM\PostRemove
+	 * @see MUTicket_Entity_Base_Supporter::performPostRemoveCallback()
+	 * @return void
+	 */
+	public function postRemoveCallback()
+	{
+		$this->performPostRemoveCallback();
+	}
 
-    /**
-     * Pre-Process the data prior to an update operation.
-     *
-     * @ORM\PreUpdate
-     * @see MUTicket_Entity_Base_Supporter::performPreUpdateCallback()
-     * @return void.
-     */
-    public function preUpdateCallback()
-    {
-        $request = new Zikula_Request_Http();
-    	$type = $request->getGet()->filter('type', 'admin', FILTER_SANITIZE_STRING);
-    	$func = $request->getGet()->filter('func', 'view', FILTER_SANITIZE_STRING);
-    	$ot = $request->getGet()->filter('ot', 'ticket', FILTER_SANITIZE_STRING);
-    	
-    	if ($type == 'admin' && $func == 'edit' && $ot == 'supporter') {
-    	$this->getSupportcats();
-    	$supportercats = $this->supportcats;
-    	$cats = serialize($supportercats);
-    	$this->setSupportcats($cats);
-    	}
-        $this->performPreUpdateCallback();
-    }
+	/**
+	 * Pre-Process the data prior to an update operation.
+	 *
+	 * @ORM\PreUpdate
+	 * @see MUTicket_Entity_Base_Supporter::performPreUpdateCallback()
+	 * @return void.
+	 */
+	public function preUpdateCallback()
+	{
+		$request = new Zikula_Request_Http();
+		$type = $request->getGet()->filter('type', 'admin', FILTER_SANITIZE_STRING);
+		$func = $request->getGet()->filter('func', 'view', FILTER_SANITIZE_STRING);
+		$ot = $request->getGet()->filter('ot', 'ticket', FILTER_SANITIZE_STRING);
+		 
+		if ($type == 'admin' && $func == 'edit' && $ot == 'supporter') {
+			$this->getSupportcats();
+			$supportercats = $this->supportcats;
+			$cats = serialize($supportercats);
+			$this->setSupportcats($cats);
+		}
+		$this->performPreUpdateCallback();
+	}
 
-    /**
-     * Post-Process the data after an update operation.
-     *
-     * @ORM\PostUpdate
-     * @see MUTicket_Entity_Base_Supporter::performPostUpdateCallback()
-     * @return void.
-     */
-    public function postUpdateCallback()
-    {
-        $this->performPostUpdateCallback();
-    }
+	/**
+	 * Post-Process the data after an update operation.
+	 *
+	 * @ORM\PostUpdate
+	 * @see MUTicket_Entity_Base_Supporter::performPostUpdateCallback()
+	 * @return void.
+	 */
+	public function postUpdateCallback()
+	{
+		$this->performPostUpdateCallback();
+	}
 
-    /**
-     * Pre-Process the data prior to a save operation.
-     *
-     * @ORM\PrePersist
-     * @ORM\PreUpdate
-     * @see MUTicket_Entity_Base_Supporter::performPreSaveCallback()
-     * @return void.
-     */
-    public function preSaveCallback()
-    {
-        $this->performPreSaveCallback();
-    }
+	/**
+	 * Pre-Process the data prior to a save operation.
+	 *
+	 * @ORM\PrePersist
+	 * @ORM\PreUpdate
+	 * @see MUTicket_Entity_Base_Supporter::performPreSaveCallback()
+	 * @return void.
+	 */
+	public function preSaveCallback()
+	{
+		$this->performPreSaveCallback();
+	}
 
-    /**
-     * Post-Process the data after a save operation.
-     *
-     * @ORM\PostPersist
-     * @ORM\PostUpdate
-     * @see MUTicket_Entity_Base_Supporter::performPostSaveCallback()
-     * @return void.
-     */
-    public function postSaveCallback()
-    {
-        $this->performPostSaveCallback();
-    }
+	/**
+	 * Post-Process the data after a save operation.
+	 *
+	 * @ORM\PostPersist
+	 * @ORM\PostUpdate
+	 * @see MUTicket_Entity_Base_Supporter::performPostSaveCallback()
+	 * @return void.
+	 */
+	public function postSaveCallback()
+	{
+		$this->performPostSaveCallback();
+	}
 
 }

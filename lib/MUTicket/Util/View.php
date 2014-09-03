44f56a6e2d1a67aa0225a1f17e90b35a1389a2b1
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2014-09-03 18:13:21 +0200

    fixed #63

--- lib/MUTicket/Util/View.php
+++ lib/MUTicket/Util/View.php
@@ -16,114 +16,122 @@
  */
 class MUTicket_Util_View extends MUTicket_Util_Base_View
 {
-	/**
-	 *
-	 * This method is for getting an array of userids of users that are
-	 * in the supporter group
-	 *
-	 * @return array
-	 */
-	public static function getSupporterIds() {
+    /**
+     *
+     * This method is for getting an array of userids of users that are
+     * in the supporter group
+     *
+     * @return array
+     */
+    public static function getSupporterIds() {
 
-		ModUtil::dbInfoLoad('Groups');
-		$tables = DBUtil::getTables();
-		$groups_column = $tables['groups_column'];
+        ModUtil::dbInfoLoad('Groups');
+        $tables = DBUtil::getTables();
+        $groups_column = $tables['groups_column'];
 
-		// get supporter group
-		$supportergroup = ModUtil::getVar('MUTicket', 'supportergroup');
-			
-		if ($supportergroup == '') {
-			$url = ModUtil::url('MUTicket', 'admin', 'config');
-			LogUtil::registerError('You have to save a supporter group!',null , $url);
-		}
+        // get supporter group
+        $supportergroup = ModUtil::getVar('MUTicket', 'supportergroup');
+        	
+        if ($supportergroup == '') {
+            $url = ModUtil::url('MUTicket', 'admin', 'config');
+            LogUtil::registerError('You have to save a supporter group!',null , $url);
+        }
 
-		$where = "WHERE $groups_column[name] = '" . DataUtil::formatForStore($supportergroup) . "'";
+        $where = "WHERE $groups_column[name] = '" . DataUtil::formatForStore($supportergroup) . "'";
 
-		// get supporter group id
-		$supportergroupid = UserUtil::getGroupIdList($where);
+        // get supporter group id
+        $supportergroupid = UserUtil::getGroupIdList($where);
 
-		// get user id's of users, which are in the supporter group
-		$supporterusersids = UserUtil::getUsersForGroup($supportergroupid);
+        // get user id's of users, which are in the supporter group
+        $supporterusersids = UserUtil::getUsersForGroup($supportergroupid);
 
-		$userids = implode(',' , $supporterusersids);
+        $userids = implode(',' , $supporterusersids);
 
-		return $userids;
+        return $userids;
 
-	}
+    }
 
-	/**
-	 *
-	 *@return array $catsupporter
-	 */
+    /**
+     *
+     *@return array $catsupporter
+     */
 
-	public static function getExistingSupporterForCategories($categoryid) {
+    public static function getExistingSupporterForCategories($categoryid) {
 
-		// Get uids of existing supporters
-		//$supporteruids = MUTicket_Util_Model::getExistingSupporterUids();
-		$repository = MUTicket_Util_Model::getSupporterRepository();
-		$supporter = $repository->selectWhere();
+        // Get uids of existing supporters
+        //$supporteruids = MUTicket_Util_Model::getExistingSupporterUids();
+        $repository = MUTicket_Util_Model::getSupporterRepository();
+        $supporter = $repository->selectWhere();
 
-		foreach ($supporteruids as $supporteruid) {
+        foreach ($supporteruids as $supporteruid) {
 
-			if(in_array($supporteruid, $categoryid)) {
-				$catsupporter[] = $supporteruid;
-			}
-		}
+            if(in_array($supporteruid, $categoryid)) {
+                $catsupporter[] = $supporteruid;
+            }
+        }
 
-		return $catsupporter;
-	}
+        return $catsupporter;
+    }
 
 
-	/**
-	 *
-	 * this method is for checking if an supporter may create a ticket or if a user may rate
-	 * 
-	 * @param $ckeck      for what we wish to check 1 = for rating, 2 for creating tickets
-	 *
-	 * return int.
-	 */
-	public static function userForRating($check = 1) {
+    /**
+     *
+     * this method is for checking if an supporter may create a ticket or if a user may rate
+     *
+     * @param $ckeck      for what we wish to check 1 = for rating, 2 for creating tickets
+     *
+     * return int.
+     */
+    public static function userForRating($check = 1) {
 
-	    $supporterTickets = '';
-	    
-		if ($check == 2) {
-			$supporterTickets = ModUtil::getVar('MUTicket', supporterTickets);
-			if ($supporterTickets == 1) {
-				$kind = 1;
-				return $kind;
-			}
-		}
+        $supporterTickets = '';
+         
+        if ($check == 2) {
+            $supporterTickets = ModUtil::getVar('MUTicket', supporterTickets);
+            if ($supporterTickets == 1) {
+                $kind = 1;
+                return $kind;
+            }
+        }
 
-		// get the supporterids
-		$supporterids = MUTicket_Util_Model::getExistingSupporterUids();
+        // get the supporterids
+        $supporterids = MUTicket_Util_Model::getExistingSupporterUids();
 
-		// get actual userid
-		$userid = UserUtil::getVar('uid');
-		if (in_array($userid, $supporterids)) {
-			$kind = 0;
-		}
-		else {
-			$kind = 1;
-		}
-		return $kind;
-	}
+        // get actual userid
+        $userid = UserUtil::getVar('uid');
+        if (in_array($userid, $supporterids)) {
+            $kind = 0;
+        }
+        else {
+            $kind = 1;
+        }
+        return $kind;
+    }
 
-	/**
-	 *
-	 * this method checks if there is a supporter saved and is active
-	 *
-	 **/
+    /**
+     *
+     * this method checks if there is a supporter saved and is active
+     * if the actual user is no supporter we block the support function else not
+     *
+     **/
 
-	public static function checkIfSupporters() {
+    public static function checkIfSupporters() {
 
-		$repository = MUTicket_Util_Model::getSupporterRepository();
-		$where = 'tbl.state = 1';
-		$supporters = $repository->selectWhere($where);
-		if (count($supporters)  > 0 && is_array($supporters)) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
+        $repository = MUTicket_Util_Model::getSupporterRepository();
+        $where = 'tbl.state = 1';
+        $supporters = $repository->selectWhere($where);
+        if (count($supporters)  > 0 && is_array($supporters)) {
+            return 1;
+        }
+        else {
+            $supporterIds = self::getSupporterIds();
+            $supporterIds = explode(',', $supporterIds);
+            $userid = UserUtil::getVar('uid');
+            if (!in_array($userid, $supporterIds)) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
 }
